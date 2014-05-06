f697c2b821b38020e1e8500224cfe69702e865ad
Jeff Wintersinger <jeff.git@wintersinger.com>
2014-05-06 09:12:57 -0600

    Begin post model.

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,31 @@
+# encoding: utf8
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, to_field='id', editable=False)),
+                ('title', models.CharField(max_length=100)),
+                ('body', models.TextField()),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('slug', models.SlugField(unique=True)),
+            ],
+            options={
+                'ordering': [b'-created_at'],
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+  user = models.ForeignKey(User, editable=False)
+  title = models.CharField(max_length = 100)
+  body = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  slug = models.SlugField(unique=True)
+
+  def __str__(self):
+    return self.title
+
+  class Meta:
+    ordering = ['-created_at']
