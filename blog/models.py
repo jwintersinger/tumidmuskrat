789ee526da28641ffa46bf75a773bca4a56afc34
Jeff Wintersinger <jeff.git@wintersinger.com>
2014-05-06 12:03:38 -0600

    Rename field.

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-  user = models.ForeignKey(User, editable=False)
+  author = models.ForeignKey(User, editable=False)
   title = models.CharField(max_length = 100)
   body = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True)
